306081e12cad32043a41e3b5d2d1574eda251a52
Jianishi <jianishi@usc.edu>
2018-07-29 12:10:04 -0700

    new

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,25 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mr<-NULL
+  get <- function() x
+  setmr<-function(x)  {mr<<-x}
+  cachemr<-function() mr
+  list(get=get,setmr=setmr,cachemr=cachemr)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  mr<-x$cachemr()
+if(!is.null(mr)) {
+  message("getting cached data")
+  return(mr)
+}
+data<-x$get()
+mare <- solve(data)
+fma$setmr(mare)
+mare
+       
 }
